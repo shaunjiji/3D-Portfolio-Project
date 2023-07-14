aded266ad0c7eab87de2cc97a32c780ecab51aa1
Shaun Jiji <shaunjiji123@gmail.com>
2023-07-14 15:30:21 -0400

    work on nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { styles } from "./styles.js";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [active, setActive] = useState("");
+
+  return (
+    <nav className="{`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}">
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img
+            src={logo}
+            alt="logo"
+            className="w-9 h-9 object-contain"
+            width="50px"
+            height="50px"
+          />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            <span className="sm:block hidden">Shaun Jiji</span>
+          </p>
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
