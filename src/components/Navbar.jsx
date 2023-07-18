cc0153185f7830d1714418f9b69a56a8e4f577d8
Shaun Jiji <shaunjiji123@gmail.com>
2023-07-18 16:12:54 -0400

    Resolve padding issue in nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-import { styles } from "./styles.js";
+import styles from "./styles.js";
 import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 
@@ -10,7 +10,9 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className="{`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}">
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
           to="/"
