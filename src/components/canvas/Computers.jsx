7334e7220161266ffc32e721f2bd5fe69ba88289
Shaun Jiji <shaunjiji123@gmail.com>
2023-07-21 10:35:42 -0400

    add useEffect in Computers component to implement mobile sizing

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -30,6 +30,24 @@ const Computers = () => {
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500)");
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
       frameLoop="demand"
